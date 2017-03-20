9bc6ce019913caed7ef0d20a163be617777420d5
lizekang <zekangli@hustunique.com>
2017-03-20 19:50:57 +0800

    fix a bug

--- homework3.py
+++ homework3.py
@@ -26,8 +26,8 @@ class Proc(object):
 
 
 def diff_proc(new_proc, old_proc):
-    create_list = []
-    terminate_list = []
+    create_list = list()
+    terminate_list = list()
     new_pid = [i.pid for i in new_proc]
     old_pid = [j.pid for j in old_proc]
     for n in new_pid:
@@ -40,7 +40,7 @@ def diff_proc(new_proc, old_proc):
 
 
 def get_proc():
-    proc_list = []
+    proc_list = list()
     for filename in os.listdir("/proc"):
         filepath = os.path.join("/proc", filename)
         if Proc.check_proc(filename):
@@ -49,15 +49,16 @@ def get_proc():
                 stat = open("/proc/{}/stat".format(pid), "r")
             else:
                 continue
-            proc = Proc(pid, stat.read().split()[1])
+            proc = Proc(pid, stat.read().split()[1].replace('(', '"').replace(')', '"'))
             proc_list.append(proc)
             stat.close()
+
     return proc_list
 
 
 def format_output_create_proc(procs, fd):
     for proc in procs:
-        if os.path.exists("/proc/{}/stat".format(proc.pid), "r"):
+        if os.path.exists("/proc/{}/stat".format(proc.pid)):
             stat = open("/proc/{}/stat".format(proc.pid), "r")
         else:
             continue
@@ -66,16 +67,17 @@ def format_output_create_proc(procs, fd):
         start_time = float(stat.read().split()[21])
         boot_time = float(uptime.read().split()[0])
         now_time = time.time()
-        fd.write("create: pid:{} name:{} create_time:{}\n"
+        fd.write("create pid:{} name:{} create_time:{}\n"
                  .format(proc.pid,
                          proc.name,
-                         time.strftime("%Y-%m-%d/%H:%M:%S", now_time - boot_time + start_time)))
+                         time.strftime("%Y-%m-%d/%H:%M:%S", time.localtime(now_time - boot_time + start_time))))
         stat.close()
         uptime.close()
 
+
 def format_output_terminate_proc(procs, fd):
     for proc in procs:
-        fd.write("terminate: pid:{} name:{} termibate_time:{}\n"
+        fd.write("terminate pid:{} name:{} terminate_time:{}\n"
                  .format(proc.pid,
                          proc.name,
                          time.strftime("%Y-%m-%d/%H:%M:%S", time.localtime(time.time()))))
@@ -84,9 +86,7 @@ def format_output_terminate_proc(procs, fd):
 pid = os.fork()
 if pid > 0:
     sys.exit(0)
-else:
-    print("fork error.")
-    exit(1)
+
 os.chdir('/')
 os.setsid()
 os.umask(0)
@@ -94,20 +94,20 @@ os.umask(0)
 pid = os.fork()
 if pid > 0:
     sys.exit(0)
-else:
-    print("fork error.")
-    exit(1)
 
-os.close(0)
-sys.stdin = open("/dev/null")
-os.close(1)
-sys.stdout = open("/dev/null", "w")
-os.close(2)
-sys.stderr = open("/dev/null", "w")
+sys.stdout.flush()
+sys.stderr.flush()
+si = open("/dev/null", 'r')
+so = open("/dev/null", 'a+')
+se = open("/dev/null", 'a+')
+os.dup2(si.fileno(), sys.stdin.fileno())
+os.dup2(so.fileno(), sys.stdout.fileno())
+os.dup2(se.fileno(), sys.stderr.fileno())
 
 proc_list = get_proc()
-fd = open("/home/lzk/Documents/gitProjects/OSlearning/homeword3.out", "w")
+fd = open("/home/lzk/Documents/gitProject/OSlearning/homework3.out", "w")
 format_output_create_proc(proc_list, fd)
+
 while True:
     time.sleep(0.5)
     new_procs = get_proc()
